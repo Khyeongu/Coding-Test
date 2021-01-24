f618c5e73d1b4f497ac56b36a68878a69699d9bc
KHG <skidrom1@gmail.com>
2021-01-24 17:29:24 +0900

    수정

--- python/그리디/1이 될 때 까지.py
+++ python/그리디/1이 될 때 까지.py
@@ -5,11 +5,15 @@ n, k=map(int, stdin.readline().rstrip().split())
 count=0
 
 while n>1 :
-    if n%k==0 :
-        n=n//k
-        count=count+1
-    else :
-        n=n-1
-        count=count+1
+    target=(n//k)*k
+    count=count+(n-target)
+    n=target
 
+    if n<k:
+        break
+
+    n=n//k
+    count+=1
+
+count=count+(n-1)
 print(count)
